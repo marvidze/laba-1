6b6cf7c6e5198c7e3e5f824a26971635cf983a29
Marat <maratayup@yandex.ru>
2025-02-07 16:20:05 +0300

    Сделал логику работы со списком

--- laba-1/MainForm.cs
+++ laba-1/MainForm.cs
@@ -12,7 +12,7 @@ namespace laba_1
 {
     public partial class MainForm : Form
     {
-        private static List<Library> objectList = new List<Library>();
+        
         public MainForm()
         {
             InitializeComponent();
@@ -20,17 +20,57 @@ namespace laba_1
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            objectList.Add(new Library("Library 1"));
-            formObjectsList.Items.AddRange(objectList.ToArray());
-        }
-
-        private void buttonCreateObject_Click(object sender, EventArgs e)
-        {
-
+            Library.objectList.Add(new Library());
+            formObjectsList.Items.AddRange(Library.objectList.ToArray());
         }
         private void buttonExit_Click(object sender, EventArgs e)
         {
             Close();
+        }
+        private void buttonCreateObjectOneParametr_Click(object sender, EventArgs e)
+        {
+            Library.objectList.Add(new Library());
+            formObjectsList.Items.Clear();
+            formObjectsList.Items.AddRange(Library.objectList.ToArray());
+        }
+        private void buttonCreateObjectNullRapametr_Click(object sender, EventArgs e)
+        {
+            Library.objectList.Add(new Library());
+            formObjectsList.Items.Clear();
+            formObjectsList.Items.AddRange(Library.objectList.ToArray());
+        }
+        private void buttonCreateObjectTwoRapametr_Click(object sender, EventArgs e)
+        {
+            Library.objectList.Add(new Library());
+            formObjectsList.Items.Clear();
+            formObjectsList.Items.AddRange(Library.objectList.ToArray());
+        }
+
+        private void buttonCreateObjectAllRapametrs_Click(object sender, EventArgs e)
+        {
+            Library.objectList.Add(new Library());
+            formObjectsList.Items.Clear();
+            formObjectsList.Items.AddRange(Library.objectList.ToArray());
+        }
+        private void buttonDeleteObject_Click(object sender, EventArgs e)
+        {
+            Library a = (Library)formObjectsList.SelectedItem;
+            Library.objectList.Remove(a);
+            a = null;
+            formObjectsList.Items.Remove(formObjectsList.SelectedItem);
+        }
+
+        private void formListObjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Library a = (Library)formObjectsList.SelectedItem;
+            //string[] arr = { "Название: "+a.Name,
+            //    "Адрес: " + a.Addres,
+            //    "Рейтинг: "+ a.Rating.ToString(),
+            //    "Номер телефона: "+ a.NumberOfPhone.ToString(),
+            //    "Количество книг: " + a.CountOfBooks.ToString(),
+            //    "Количество мест в читальном зале: " + a.CountOfSeats.ToString()};
+            //formListInfoObject.Items.Clear();
+            //formListInfoObject.Items.AddRange(arr);
         }
     }
 }
