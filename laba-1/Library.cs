728e7842c37e3714ee50f22320661dde7d679cfe
Marat <maratayup@yandex.ru>
2025-02-10 21:52:51 +0300

    сделал исключения

--- laba-1/Library.cs
+++ laba-1/Library.cs
@@ -11,7 +11,7 @@ namespace laba_1
     public class Library
     {
         private string _name;
-        private string _addres = "Отсутствует значение";
+        private string _addres = "";
         private string _numberOfPhone = "";
         private float _rating = 0;
         private int _countOfBooks = 0;
@@ -21,35 +21,55 @@ namespace laba_1
         private static List<Library> _objectList = new List<Library>();
 
         public string Name { get => _name; set => _name = value; }
-        public string Addres { get => _addres; set => _addres = value; }
-        public string NumberOfPhone { get => _numberOfPhone; set => _numberOfPhone = value; }
+        public string Addres {  get => _addres == "" ? throw new LibraryException("Отсутствует значение!") : _addres; 
+            set => _addres = value; }
+        public string NumberOfPhone { get => _numberOfPhone == "" ? throw new LibraryException("Отсутствует значение!") : _numberOfPhone;
+            set => _numberOfPhone = value; }
         public float Rating { get => _rating; set => _rating = value; }
         public int CountOfBooks { get => _countOfBooks; set => _countOfBooks = value; }
         public int CountOfSeats { get => _countOfSeats; set => _countOfSeats = value; }
         public int Id { get => _id; set => _id = value; }
-        public static int CountOfObjects { get => _countOfObjects; set => _countOfObjects = value; }
         public static List<Library> ObjectList { get => _objectList; set => _objectList = value; }
+        public static int CountOfObjects { get => _countOfObjects; set => _countOfObjects = value; }
 
+        /// <summary>
+        /// Конструктор по-умолчанию для класса Library
+        /// </summary>
         public Library()
         {
             _countOfObjects++;
             _id = _countOfBooks;
             _name = "Библиотека №" + _countOfObjects.ToString();
         }
-
-        public Library(String name)
+        /// <summary>
+        /// Конструктор с одним параметром для класса Library 
+        /// </summary>
+        /// <param name="name">Имя библиотеки</param>
+        public Library(string name)
         {
             _countOfObjects++;
             _name = name;
         }
-
-        public Library(String name, String addres)
-        {
+        /// <summary>
+        /// Конструктор с двумя параметрами для класса Library 
+        /// </summary>
+        /// <param name="name">Имя библиотеки</param>
+        /// <param name="addres">Адрес библиотеки</param>
+        public Library(string name, string addres) : this(name) 
+        { 
             _countOfObjects++;
-            _name = name;
             _addres = addres;
         }
 
+        /// <summary>
+        /// Конструктор со всеми параметрами для класса Library
+        /// </summary>
+        /// <param name="name">Имя библиотеки</param>
+        /// <param name="addres">Адрес библиотеки</param>
+        /// <param name="numberOfPhone">Номер телефона библиотеки</param>
+        /// <param name="rating">Рейтинг библиотеки</param>
+        /// <param name="countOfBooks">Количество книг в библиотеке</param>
+        /// <param name="countOfSeats">Количество мест в читальном зале</param>
         public Library(string name, string addres, string numberOfPhone, float rating, int countOfBooks, int countOfSeats) : this(name, addres)
         {
             _countOfObjects++;
@@ -66,6 +86,15 @@ namespace laba_1
         public override string ToString()
         {
             return $"{_name}";
+        }
+
+        public static void IWantToException(int count)
+        {
+            if (count > 1000)
+            {
+                throw new StackOverflowException("Стек переполнен!");
+            }
+            IWantToException(count+1);
         }
     }
 }
