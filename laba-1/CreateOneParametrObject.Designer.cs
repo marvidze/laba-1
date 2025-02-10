281c499be3736ba44f39e5331c6873b34135b8c2
Vlad Ipatov <royalevlad2@gmail.com>
2025-02-10 21:52:05 +0300

    Переделал создание объектов.

--- laba-1/CreateOneParametrObject.Designer.cs
+++ laba-1/CreateOneParametrObject.Designer.cs
@@ -28,19 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_adress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_tel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown_rating = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_books = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_place = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_rating)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_books)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_place)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(180, 30);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(142, 20);
+            this.textBox_name.TabIndex = 0;
+            this.textBox_name.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(255, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Адресс:";
+            // 
+            // textBox_adress
+            // 
+            this.textBox_adress.Location = new System.Drawing.Point(180, 60);
+            this.textBox_adress.Name = "textBox_adress";
+            this.textBox_adress.Size = new System.Drawing.Size(142, 20);
+            this.textBox_adress.TabIndex = 5;
+            this.textBox_adress.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Рейтинг:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Номер телефона:";
+            // 
+            // textBox_tel
+            // 
+            this.textBox_tel.Location = new System.Drawing.Point(180, 120);
+            this.textBox_tel.Name = "textBox_tel";
+            this.textBox_tel.Size = new System.Drawing.Size(142, 20);
+            this.textBox_tel.TabIndex = 9;
+            this.textBox_tel.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Кол-во книг:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Мест в ЧЗ:";
+            // 
+            // numericUpDown_rating
+            // 
+            this.numericUpDown_rating.Location = new System.Drawing.Point(180, 90);
+            this.numericUpDown_rating.Name = "numericUpDown_rating";
+            this.numericUpDown_rating.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown_rating.TabIndex = 14;
+            // 
+            // numericUpDown_books
+            // 
+            this.numericUpDown_books.Location = new System.Drawing.Point(180, 150);
+            this.numericUpDown_books.Name = "numericUpDown_books";
+            this.numericUpDown_books.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown_books.TabIndex = 15;
+            // 
+            // numericUpDown_place
+            // 
+            this.numericUpDown_place.Location = new System.Drawing.Point(180, 182);
+            this.numericUpDown_place.Name = "numericUpDown_place";
+            this.numericUpDown_place.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown_place.TabIndex = 16;
             // 
             // CreateOneParametrObject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 223);
+            this.ClientSize = new System.Drawing.Size(342, 267);
+            this.Controls.Add(this.numericUpDown_place);
+            this.Controls.Add(this.numericUpDown_books);
+            this.Controls.Add(this.numericUpDown_rating);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_tel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_adress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_name);
             this.Name = "CreateOneParametrObject";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Создание объекта с одним параметром";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_rating)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_books)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_place)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_adress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_tel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown_rating;
+        private System.Windows.Forms.NumericUpDown numericUpDown_books;
+        private System.Windows.Forms.NumericUpDown numericUpDown_place;
     }
 }
--- laba-1/CreateOneParametrObject.cs
+++ laba-1/CreateOneParametrObject.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,25 @@ namespace laba_1
         public CreateOneParametrObject()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Name = textBox_name.Text;
+            string Adress = textBox_adress.Text;
+            float Rating = (float)numericUpDown_rating.Value;
+            string Tel = textBox_tel.Text;
+            int Books = (int)numericUpDown_books.Value;
+            int Place = (int)numericUpDown_place.Value;
+            Library.ObjectList.Add(new Library(Name, Adress, Tel, Rating, Books, Place));
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
         }
     }
 }
