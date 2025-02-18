49ee9cbbda8a4594145075ce35717cb1b673f6fd
Marat Ayupov <maratayup@yandex.ru>
2025-02-18 12:11:19 +0300

    Изменил get set и поля

--- laba-1/Library.cs
+++ laba-1/Library.cs
@@ -10,53 +10,45 @@ namespace laba_1
 {
     public class Library
     {
-        private string _name;
-        private string _addres = "Отсутствует значение";
-        private string _numberOfPhone = "Отсутствует значение";
-        private float _rating = 0;
-        private int _countOfBooks = 0;
-        private int _countOfSeats = 0;
-        private int _id;
-        private static int _countOfObjects = 0;
         private static List<Library> _objectList = new List<Library>();
 
-        public string Name { get => _name; set => _name = value; }
-        public string Addres { get => _addres; set => _addres = value; }
-        public string NumberOfPhone { get => _numberOfPhone; set => _numberOfPhone = value; }
-        public float Rating { get => _rating; set => _rating = value; }
-        public int CountOfBooks { get => _countOfBooks; set => _countOfBooks = value; }
-        public int CountOfSeats { get => _countOfSeats; set => _countOfSeats = value; }
-        public int Id { get => _id; set => _id = value; }
-        public static int CountOfObjects { get => _countOfObjects; set => _countOfObjects = value; }
+        public string Name { get; set; }
+        public string Addres { get ; set; } = "Отсутствует значение";
+        public string NumberOfPhone { get ; set; } = "Отсутствует значение";
+        public float Rating { get ; set ; } = 0;
+        public int CountOfBooks { get; set; } = 0;
+        public int CountOfSeats { get; set; } = 0;
+        public int Id { get; set; } = 0;
+        public static int CountOfObjects { get; set; } = 0;
         public static List<Library> ObjectList { get => _objectList; set => _objectList = value; }
 
         public Library()
         {
-            _countOfObjects++;
-            _id = _countOfBooks;
-            _name = "Библиотека №" + _countOfObjects.ToString();
+            CountOfObjects++;
+            Id++;
+            Name = "Библиотека №" + CountOfObjects.ToString();
         }
 
         public Library(String name)
         {
-            _countOfObjects++;
-            _name = name;
+            CountOfObjects++;
+            Name = name;
         }
 
         public Library(String name, String addres)
         {
-            _countOfObjects++;
-            _name = name;
-            _addres = addres;
+            CountOfObjects++;
+            Name = name;
+            Addres = addres;
         }
 
         public Library(string name, string addres, string numberOfPhone, float rating, int countOfBooks, int countOfSeats) : this(name, addres)
         {
-            _countOfObjects++;
-            _numberOfPhone = numberOfPhone;
-            _rating = rating;
-            _countOfBooks = countOfBooks;
-            _countOfSeats = countOfSeats;
+            CountOfObjects++;
+            NumberOfPhone = numberOfPhone;
+            Rating = rating;
+            CountOfBooks = countOfBooks;
+            CountOfSeats = countOfSeats;
         }
 
         /// <summary>
@@ -65,7 +57,7 @@ namespace laba_1
         /// <returns>Возвращает строку, содержащую всю информацию о библиотеке.</returns>
         public override string ToString()
         {
-            return $"{_name}";
+            return $"{Name}";
         }
     }
 }
