f7fd5beaad30e00ec2d4c35e44bfbd7623576e08
Marat <maratayup@yandex.ru>
2025-02-23 10:13:39 +0300

    тесты

---
+++ laba-1Tests/LibraryTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using laba_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1.Tests
+{
+    [TestClass()]
+    public class LibraryTests
+    {
+        [TestMethod()]
+        public void IWantToTestTest()
+        {
+            int x = 1;
+            int y = 2;
+            int expected = 3;
+
+            int actual = Library.IWantToTest(x, y);
+            Assert.AreEqual(expected, actual);
+
+            
+        }
+    }
+}
