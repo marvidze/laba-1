b4112e1a3d349df3105e4844807b861ee4867442
Marat <maratayup@yandex.ru>
2025-03-10 18:28:25 +0300

    Рефакторинг (на паттерн Builder)

---
+++ laba-1/LibraryBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1
+{
+    internal class LibraryBuilder
+    {
+        private Library _library = new Library();
+
+        public LibraryBuilder SetName(string name)
+        {
+            _library.Name = name;
+            return this;
+        }
+
+        public LibraryBuilder SetAddress(string address)
+        {
+            _library.Addres = address;
+            return this;
+        }
+
+        public LibraryBuilder SetNumberOfPhone(string numberOfPhone)
+        {
+            _library.NumberOfPhone = numberOfPhone;
+            return this;
+        }
+
+        public LibraryBuilder SetRating(float rating)
+        {
+            _library.Rating = rating;
+            return this;
+        }
+
+        public LibraryBuilder SetCountOfBooks(int countOfBooks)
+        {
+            _library.CountOfBooks = countOfBooks;
+            return this;
+        }
+
+        public LibraryBuilder SetCountOfSeats(int countOfSeats)
+        {
+            _library.CountOfSeats = countOfSeats;
+            return this;
+        }
+
+        public Library Build()
+        {
+            return _library;
+        }
+    }
+}
